0078fe4215f9b2ec301c8485f308a24e961bf51f
dieggoh <dieggoh@gmail.com>
2015-02-09 12:57:22 -0300

    Primera modificación

---
+++ testConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace testConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hola");
+            Console.Write(" Diego");
+                
+          
+        }
+    }
+}
